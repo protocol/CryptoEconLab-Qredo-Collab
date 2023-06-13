19e3353d3d13555e1ef3834f4ab6138814128832
Maria Silva <misilva73@gmail.com>
2023-06-13 16:44:54 +0300

    Update supply and sim modules

--- mechaqredo/sim.py
+++ mechaqredo/sim.py
@@ -1,1 +1,24 @@
-from price import Pricefrom staking import *from trasactions import ServiceFees,BurnFeesfrom params import *from vesting import Vestingfrom staking import Stakingfrom supply import CircSupply"initialise the simulation"Vesting=Vesting()CircSupply=CircSupply(C0=0)for t in range(T):        #update vesting    Vesting.update()        #compute Burns    Burns.update()        #updates price    Price.update()            #Staking part:    #Locks tokens    Staking.locks()    #Unlocked tokens    Staking.unlock()        #updates circulating supply    CircSupply.update(Vesting,Burns,Staking,Price)                                    
+import pandas as pd
+
+from params import validate_params_dict
+from data_models import build_model_data_dict
+from supply import forecast_supply_stats
+
+
+def run_single_sim(forecast_length: int, input_params_dict: dict) -> pd.DataFrame:
+    # Validate input parameters
+    params_dict = validate_params_dict(input_params_dict)
+    # Build
+    data_dict = build_model_data_dict(forecast_length, params_dict)
+    # Forecast supplt stats
+    supply_data_dict = forecast_supply_stats(forecast_length, params_dict, data_dict)
+    # Build output dataframe
+    df = build_output_dataframe(data_dict, supply_data_dict)
+    return df
+
+
+def build_output_dataframe(data_dict: dict, supply_data_dict: dict) -> pd.DataFrame:
+    data_df = pd.DataFrame(data_dict)
+    supply_data_df = pd.DataFrame(supply_data_dict)
+    df = pd.concat([supply_data_df, data_df], axis=1)
+    return df
--- mechaqredo/supply.py
+++ mechaqredo/supply.py
@@ -1,45 +1,80 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  7 23:07:28 2023
-@author: juanpablomadrigalcianci
-"""
+import numpy as np
 
-class CircSupply:
-    def __init__(self, initial_circulating_supply:float, initial_price:float):
-        # Initializing circulating supply, inflation rate, and market capitalization lists
-        self.circ_supply_list = [initial_circulating_supply]
-        self.inflation_rate_list = [0]
-        self.market_capitalization_list = [initial_circulating_supply * initial_price]
-        
-    def update(self, vesting, burns, staking, price):
-        """Update supply, market capitalization, and inflation rate.
-            Here, vesting, burns,staking and price are the modules 
-            that represent these actions
-        """
-        self.circ_supply_list.append(self._updateSupply(vesting, burns, staking))
-        self.market_capitalization_list.append(self._updateMarketCap(price))
-        self.inflation_rate_list.append(self._updateInflationRate())
-
-    def current_supply(self):
-        """Return the current supply."""
-        return self.circ_supply_list[-1]
-    
-    def _updateSupply(self, vesting, burns, staking):
-        """Update the new supply."""
-        new_supply = self.current_supply()
-        new_supply += -burns.current_burn() + staking.current_out()
-        new_supply += -staking.current_in() + vesting.current_vesting()
-        return new_supply
-    
-    def _updateMarketCap(self, price):
-        """Update the market capitalization."""
-        return price.current_price() * self.current_supply()
-    
-    def _updateInflationRate(self):
-        """Update the inflation rate."""
-        return (self.current_supply() - self.circ_supply_list[-2]) / self.circ_supply_list[-2]
+from vesting import forecast_vested_vec
+from locking import (
+    forecast_service_fee_locked_vec,
+    forecast_staking_rewards_and_ecosystem_fund,
+)
 
 
-
-    
+def forecast_supply_stats(
+    forecast_length: int, params_dict: dict, data_dict: dict
+) -> dict:
+    # Get inputs from data dict
+    n_txs_vec = data_dict["n_txs_vec"]
+    token_price_vec = data_dict["token_price_vec"]
+    service_fees_vec = data_dict["service_fees_vec"]
+    staking_inflows_vec = data_dict["staking_inflows_vec"]
+    staking_outflows_vec = data_dict["staking_outflows_vec"]
+    n_val_vec = data_dict["n_val_vec"]
+    # Forecast burned tokens
+    burn_params_dict = params_dict["burning"]
+    burn_extra_vec = burn_params_dict["burn_extra_vec"]
+    protocol_fee_rate = burn_params_dict["protocol_fee_rate"]
+    burn_fees_vec = protocol_fee_rate * n_txs_vec
+    burned_vec = burn_extra_vec + burn_fees_vec
+    # Forecast vested tokens
+    vested_vec = forecast_vested_vec(forecast_length, params_dict)
+    # Forecast locked tokens
+    service_fee_locked_vec = forecast_service_fee_locked_vec(
+        params_dict,
+        token_price_vec,
+        service_fees_vec,
+    )
+    locked_vec = service_fee_locked_vec + staking_inflows_vec
+    # Forecast released tokens
+    protocol_funded_rate = params_dict["staking"]["protocol_funded_rate"]
+    ecosystem_fund_zero = params_dict["ecosystem_fund_zero"]
+    #   Get token releases from protocol fees covered by the protocol
+    released_protocol_burn_vec = protocol_funded_rate * burned_vec
+    #   Get token releases from staking rewards
+    (
+        released_staking_rewards_vec,
+        ecosystem_fund_vec,
+    ) = forecast_staking_rewards_and_ecosystem_fund(
+        forecast_length,
+        service_fee_locked_vec,
+        released_protocol_burn_vec,
+        staking_inflows_vec,
+        staking_outflows_vec,
+        n_val_vec,
+        ecosystem_fund_zero,
+    )
+    #   Bring all releases together
+    released_vec = (
+        released_protocol_burn_vec + released_staking_rewards_vec + staking_outflows_vec
+    )
+    # Compute circulating supply
+    circ_supply_zero = params_dict["circ_supply_zero"]
+    circ_supply = (
+        circ_supply_zero
+        - burned_vec.cumsum()
+        + vested_vec.cumsum()
+        - locked_vec.cumsum()
+        + released_vec.cumsum()
+    )
+    # Put tofether output dict
+    output_dict = {
+        "iteration": np.arange(0, forecast_length, 1),
+        "circ_supply": circ_supply,
+        "day_burned_vec": burned_vec,
+        "day_vested_vec": vested_vec,
+        "day_locked_vec": locked_vec,
+        "day_released_vec": released_vec,
+        "day_inflation": np.diff(circ_supply) / circ_supply[:-1],
+        "market_cap": circ_supply * token_price_vec,
+        "day_burn_fees_vec": burn_fees_vec,
+        "day_service_fee_locked_vec": service_fee_locked_vec,
+        "ecosystem_fund_vec": ecosystem_fund_vec,
+    }
+    return output_dict
