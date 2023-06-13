1cc5d30db4d9b14c16ef0b246824c83d9976e795
Maria Silva <misilva73@gmail.com>
2023-06-13 17:22:25 +0300

    Add dummy vesting function

--- mechaqredo/vesting.py
+++ mechaqredo/vesting.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
-def forecast_vested_vec(forecast_length: int, parms_dict: dict) -> np.array:
-    # TODO: implement vesting module
-    return np.ones(forecast_length)
+def forecast_vested_vec(forecast_length: int, params_dict: dict) -> np.array:
+    # TODO: Improve vesting module
+    total_to_vest = 1600000000
+    vesting_days = 365 * 3
+    daily_vest = total_to_vest / vesting_days
+    return np.ones(forecast_length) * daily_vest
